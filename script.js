83b2345d256b2b0ef73a20b0b86dbfdcf5f9f248
Adi Meirovitch <adime.support@gmail.com>
2017-11-05 13:22:35 +0200

    moving the innerHTML inside the click function

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ $(document).ready(function() {
       var num3 = parseInt($("#num3").val());
       var arrNum = arrangeBySize(num1, num2, num3);
   
-      // Deal with the output
-      alert(arrNum);
+      document.getElementById("show").innerHTML = arrNum;
     });
 });
 
@@ -26,7 +25,7 @@ function arrangeBySize(nm1, nm2, nm3) {
       else if (nm2 >= nm1 && nm1>nm3) {
     return   [nm2,nm1,nm3];}
     else if (nm3 >= nm2 && nm2>nm1) {
-      return   [nm2,nm2,nm1];
+      return   [nm3,nm2,nm1];
     }
     else if (nm1 >= nm3 && nm2>nm2) {
       return   [nm1,nm3,nm2];}
